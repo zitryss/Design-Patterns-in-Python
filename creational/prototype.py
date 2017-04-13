78839da6b7a1eb4eb26a8826206cacc345ad66af
Eugene Russkikh <zitryss@gmail.com>
2017-04-13 11:48:16 +0200

    Add assert check

--- creational/prototype.py
+++ creational/prototype.py
@@ -17,6 +17,7 @@ class Prototype:
 def main():
     prototype = Prototype()
     prototype_copy = copy.deepcopy(prototype)
+    assert prototype is not prototype_copy
 
 
 if __name__ == "__main__":
